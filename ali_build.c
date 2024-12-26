1359b5ab1dffe86bbdcff5ccdba1c0201671b5e0
spiel0meister <soviczan7@gmail.com>
2024-12-26 22:13:31 +0100

    updated ali_build

--- ali_build.c
+++ ali_build.c
@@ -2,16 +2,24 @@
 #define ALI_IMPLEMENTATION
 #include "ali.h"
 
+static AliArena string_arena = {0};
+
 int main(int argc, char** argv) {
     char** cmd = NULL;
     rebuild_yourself(&cmd, argc, argv);
 
-    CexeBuilder exe = c_exe("main", "main.c");
-    c_exe_add_flag(&exe, "-Wall");
-    c_exe_add_flag(&exe, "-Wextra");
-    c_exe_add_flag(&exe, "-Werror");
-    c_exe_add_flag(&exe, "-ggdb");
-    if (!c_exe_execute(&cmd, &exe)) return 1;
+    AliCBuilder builder = {0};
+
+    AliCBuilder* foo_obj = ali_c_builder_next_subbuilder(&builder);
+    c_builder_reset(foo_obj, C_OBJ, &string_arena, "foo.o", "foo.c");
+    c_builder_add_flags(foo_obj, "-Wall", "-Wextra", "-ggdb");
+
+    c_builder_reset(&builder, C_EXE, &string_arena, "main", "main.c");
+    c_builder_add_flags(&builder, "-Wall", "-Wextra", "-ggdb");
+    if (!c_builder_execute(&builder, &cmd)) return 1;
+
+    da_free(cmd);
+    arena_free(&string_arena);
 
     return 0;
 }
