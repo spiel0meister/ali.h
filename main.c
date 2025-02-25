82bb9704eaf6a3dc8f429440b752419c6a1ff9e9
spiel0meister <soviczan7@gmail.com>
2025-02-25 17:05:07 +0100

    changes III

--- main.c
+++ main.c
@@ -3,18 +3,10 @@
 
 #define ALI_IMPLEMENTATION
 #define ALI_REMOVE_PREFIX
-#define ALI_INTERNAL_TESTING
 #include "ali.h"
 
-#define ALI_MATH_IMPLEMENTATION
-#define ALI_MATH_REMOVE_PREFIX
-#include "ali_math.h"
-
-#define ALI_NET_IMPLEMENTATION
-#define ALI_NET_REMOVE_PREFIX
-#include "ali_net.h"
-
 int main(void) {
+    logn_info("Hello, World");
     return 0;
 }
 
