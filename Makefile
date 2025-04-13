e7f101f415ec5dde5d1f4ae094b07adf88df1db4
spiel0meister <soviczan7@gmail.com>
2025-04-13 15:27:32 +0200

    switched from make to ali2 builder

--- Makefile
+++
@@ -1,4 +0,0 @@
-CFLAGS = -Wall -Wextra -ggdb
-
-main: main.c ali2.h
-	$(CC) $(CFLAGS) -o $@ $<
---
+++ builder.c
@@ -0,0 +1,13 @@
+#define ALI2_IMPLEMENTATION
+#define ALI2_REMOVE_PREFIX
+#include "ali2.h"
+
+int main(int argc, char** argv) {
+    AliCmd cmd = {0};
+    ALI_REBUILD_YOURSELF(&cmd, argc, argv);
+
+    cmd_append_many(&cmd, "gcc", "-o", "main", "main.c");
+    if (!cmd_run_sync_and_reset(&cmd)) return 1;
+
+    return 0;
+}
